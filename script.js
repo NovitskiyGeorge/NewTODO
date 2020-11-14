da58631a7f44da617936a3517bc58f3839a65ac6
Note <novitskiy-gs@yandex.ru>
2020-11-14 19:53:55 +0300

    dev remove tasks

--- script.js
+++ script.js
@@ -1,42 +1,38 @@
-let btn = document.querySelector('.btn-task');
+let btn = document.querySelectorAll('#btn');
 let overlay = document.querySelector('.overlay');
 let modal = document.querySelector('.modal');
 let addTask = document.querySelector('.add-task');
 let task = document.querySelector('.new-task');
-
-btn.addEventListener('click', function() {
-    console.log('done');
-    overlay.style.display = 'block';
+let taskCounter = document.querySelector('.count');
 
 
+
+btn.forEach(function(item, i, arr) {
+    item.addEventListener('click', function() {
+        overlay.style.display = 'block';
+    });
 
 });
+
+
 
 let count = 0;
 
 
 function init() {
-    
-
-
 
 for(let i=0; i<localStorage.length; i++) {
+
     let key = localStorage.key(i);
     console.log((`${key}: ${localStorage.getItem(key)}`));
+
     let newTask = task.cloneNode(true);
     newTask.style.display = 'flex';
     newTask.querySelector('p').innerHTML = localStorage.getItem(key);
     task.after(newTask);    
-
   }
-
-
 count = localStorage.length;
-console.log(count);
 return count;
-
-
-
 }
 
 init();
@@ -46,22 +42,40 @@ addTask.addEventListener('click',function() {
 
     let text = document.querySelector('.text');
     let newTask = task.cloneNode(true);
+
     newTask.style.display = 'flex';
     newTask.querySelector('p').innerHTML = text.value;
     task.after(newTask);    
+
     localStorage.setItem(count, text.value);
 
     text.value = '';    
     overlay.style.display = 'none';
-
     count++;
 
+    taskCounter.innerHTML = `tasks: ${count}`;
 
 });
 
-let circle = document.querySelector('.circle');
+taskCounter.append(count);
 
-circle.addEventListener('click', function() {
-    localStorage.clear();
+
+function removeTasks() {
+
+let taskRemove = document.querySelectorAll('.new-task');
+let checkbox = document.querySelectorAll('.check');
+
+checkbox.forEach(function(item, i) {
+    item.addEventListener('change', function() {
+
+    function timeRemove() {
+        taskRemove[i].remove();
+    }
+    setTimeout(timeRemove, 300);
+
+    });
 });
 
+}
+
+removeTasks();
