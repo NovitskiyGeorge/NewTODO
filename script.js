5a88857424e89a862ff2b73b07601db8e71e72be
Note <novitskiy-gs@yandex.ru>
2020-11-14 14:39:26 +0300

    dev btn add task

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-let btn = document.querySelector('button');
-
+let btn = document.querySelector('.btn-task');
+let overlay = document.querySelector('.overlay');
+let modal = document.querySelector('.modal');
+let addTask = document.querySelector('.add-task');
+let task = document.querySelector('.new-task');
 
 btn.addEventListener('click', function() {
+    console.log('done');
+    overlay.style.display = 'block';
+
+
 
 });
+
+addTask.addEventListener('click',function() {
+    let text = document.querySelector('.text');
+    let newTask = task.cloneNode(true);
+    newTask.style.display = 'flex';
+    newTask.querySelector('p').innerHTML = text.value;
+    task.after(newTask);    
+    text.value = '';
+
+    overlay.style.display = 'none';
+});
+
 
 
 
