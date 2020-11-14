ccdc5fa9d9ae57954b1214596349ab37fc59830e
George <novitskiy.gs@gmail.com>
2020-11-14 10:44:54 +0300

    dev modal

---
+++ script.js
@@ -0,0 +1,22 @@
+let btn = document.querySelector('button');
+
+
+btn.addEventListener('click', function() {
+
+});
+
+
+
+
+
+
+
+
+
+
+
+// let steck = document.querySelector('.steck');
+// let newDiv = document.createElement('div');
+
+// newDiv.innerHTML = 'Hi!';
+// steck.prepend(newDiv);
