89c05c5de4699b334989e8ee22f6e5cb98b8a674
George <novitskiy.gs@gmail.com>
2020-11-18 10:30:50 +0300

    dev remove localStorage

--- script.js
+++ script.js
@@ -3,79 +3,59 @@ let overlay = document.querySelector('.overlay');
 let modal = document.querySelector('.modal');
 let addTask = document.querySelector('.add-task');
 let taskCounter = document.querySelector('.count');
-
 let steck = document.querySelector('.steck');
 
+let count = 0;
+let item;
+let itemsList = [];
+const STORAGE_ITEMS = 'storage-items';
 
 
 btn.forEach(function(item, i, arr) {
     item.addEventListener('click', function() {
-        overlay.style.display = 'block';
-        
+        overlay.style.display = 'block';     
     });
-
 });
 
+    
 
-
-let count = 0;
 
 
 function init() {
 
-for(let i = 0; i < localStorage.length; i++) {
-    
-    let key = localStorage.key(i);
+    itemsList = JSON.parse(localStorage.getItem(STORAGE_ITEMS));
 
+    if (itemsList) {
 
-    if(i == 0){
+    itemsList.forEach(function(item, i, arr) {
 
         let newTask = document.createElement('div');
         newTask.className = 'new-task';
         steck.append(newTask);    
     
-    
         let checkbox = document.createElement('input');
         checkbox.type = 'checkbox';
         checkbox.className = 'check';
-        newTask.append(checkbox);
-    
+        newTask.append(checkbox);    
     
         let p = document.createElement('p');
-        p.innerHTML = localStorage.getItem(key);
-        newTask.append(p);
+        p.innerHTML = item.text;
+        newTask.append(p);   
+
+        count++;
         
+    });
 
-    } else {
-        
-        let task = document.querySelector('.new-task');
-
-        let newTask = document.createElement('div');
-        newTask.className = 'new-task';
-        task.after(newTask);
-
-
-        let checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.className = 'check';
-        newTask.append(checkbox);
-
-
-        let p = document.createElement('p');
-        p.innerHTML = localStorage.getItem(key);
-        newTask.append(p);
     }
-    console.log('done!');
-  }
-    count = localStorage.length;
-    return count;
+    taskCounter.append(count);
+
+
 }
 
 init();
 
 
-addTask.addEventListener('click',function() {
-    
+addTask.addEventListener('click',function() {   
 
     let text = document.querySelector('.text');
 
@@ -83,58 +63,63 @@ addTask.addEventListener('click',function() {
     newTask.className = 'new-task';
     steck.append(newTask);    
 
-
     let checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.className = 'check';
     newTask.append(checkbox);
 
-
     let p = document.createElement('p');
     p.innerHTML = text.value;
-    newTask.append(p);
+    newTask.append(p);   
 
-    localStorage.setItem(count, text.value);
+    count++;
+    taskCounter.innerHTML = count;    
 
+    item = {
+        id: count,
+        text: text.value
+    };
+
+    if(itemsList == null) {
+        itemsList = [];
+    }
+
+    itemsList.push(item);
+    
+    localStorage.setItem(STORAGE_ITEMS, JSON.stringify(itemsList));
 
     text.value = '';    
     overlay.style.display = 'none';
-    count++;
-
-    taskCounter.innerHTML = `tasks: ${count}`;
-
 
 });
 
-taskCounter.append(count);
 
+steck.addEventListener('input', function() {
+    function removeTasks() {
+        let taskRemove = document.querySelectorAll('.new-task');
+        let checkbox = document.querySelectorAll('.check'); 
+        count--;  
+        taskCounter.innerHTML = count;      
+        console.log(itemsList);
 
-function removeTasks() {
-let taskRemove = document.querySelectorAll('.new-task');
-let checkbox = document.querySelectorAll('.check');
-
-
-
-checkbox.forEach(function(item, i) {
-    item.addEventListener('change', function() {
-
-        let key = localStorage.key(i);
-        localStorage.removeItem(key);
         
-               
+        checkbox.forEach(function(item, i) {
 
+            // debugger
+            item.addEventListener('change', function() {      
+                
+                itemsList = itemsList.filter(function(item) {
+                   return item.id !== i;
+                });
+                
 
-        setTimeout(() => {
-            
-        taskRemove[i].remove();
-            
-        }, 200);
-
-
-    
-    });
-
+                setTimeout(() => {            
+                taskRemove[i].remove();            
+                }, 200);    
+            });
+        
+        });
+        }
+        removeTasks();
 });
-}
-removeTasks();
 
